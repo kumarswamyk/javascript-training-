a7e07094e47ad07ee3cd32dd738c9a54bfc97652
kumarswamyk <68528859+kumarswamyk@users.noreply.github.com>
2022-10-11 11:52:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-training-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-bgvt7r)
---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('First java script');
+if (true) {
+  var id = 100; //let follows scope variable
+  //let id=100;
+}
+console.log(id);
